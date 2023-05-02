bc6d6112a7766bfd938dc9db1e1676778d0b7f09
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-05-02 21:53:43 +0530

    done

--- src/Components/PageNotFound/Pagenotfound.jsx
+++ src/Components/PageNotFound/Pagenotfound.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./pagenotfound.css"
+import { Link } from 'react-router-dom'
+
 
 const Pagenotfound = () => {
   return (
-    <div>Pagenotfound</div>
+    <div className='wrongPage'>
+      <h1 className='wrongPathheading'>4 <span><i class="fa-solid fa-angle-left"></i> / <i class="fa-solid fa-angle-right"></i></span> 4</h1>
+      <p className='pOne'>Error404 = <span>()</span> =<i class="fa-solid fa-angle-right"></i></p>
+      <p className='pTwo'>message <span className='span'>=</span> <span className='spanOne'>"page not found";</span></p>
+      
+      <Link  to="/home"><button className='backButton'>Home</button></Link>
+    </div>
   )
 }
+
+// rgb(123,160,102)
 
 export default Pagenotfound
